8a7f21cffbe1912cca512d1a7fdcbaac4cebe61a
tonCabrera <adjoilton@gmail.com>
2024-08-09 15:05:07 -0300

    Add files via upload

---
+++ Aula05.py
@@ -0,0 +1,82 @@
+# Exercícios: 
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+print("Exercício 1")
+print("Seja Bem Vindo")
+
+# 2 Declare uma variável booleana verdadeiro
+print("Exercício 2")
+booleana_verdadeiro = True
+
+# com o valor True e imprima seu tipo
+print(booleana_verdadeiro)
+print(type(booleana_verdadeiro))
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+print("Exercício 3")
+número_decimal1 = 10.5
+número_decimal2 = 2.3
+multiplicacao = número_decimal1 * número_decimal2
+print(multiplicacao)
+
+## 4 Imprima o resultado da divisão de dois números inteiros de sua escolha.
+print("Exercício 4")
+número_inteiro1 = 10
+número_inteiro2 = 2
+divisao = int(número_inteiro1 / número_inteiro2)
+print(divisao)
+
+## 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+print("Exercício 5")
+número_inteiro1 = 10
+número_inteiro2 = 2
+subtracao = número_inteiro1 - número_inteiro2
+print(subtracao)
+
+# 6 Imprima o resultado da divisão inteira de dois números inteiros de sua escolha.
+print("Exercício 6")
+numero_inteiro1 = 3
+numero_inteiro2 = 2
+divisao = int(numero_inteiro1 / numero_inteiro2)
+print(divisao)
+
+# 7 Imprima o resultado da multiplicação de quatro números decimais de sua escolha
+print("Exercício 7")
+número_decimal1 = 10
+número_decimal2 = 2
+número_decimal3 = 3
+número_decimal4 = 5
+multiplicacao = número_decimal1 * número_decimal2 * número_decimal3 * número_decimal4
+print(multiplicacao)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+print("Exercício 8")
+numero_inteiro = 5
+dobro = numero_inteiro*2
+print(dobro)
+
+# 9 Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel)
+print("Exercício 9")
+
+numero1 = float(input("Digite o primeiro número a somar: "))
+numero2 = float(input("Digite o segundo número a somar: "))
+
+soma = numero1 + numero2
+
+print(soma)
+
+# 10 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel)
+print("Exercício 10")
+
+nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+idade = input("Digite a sua idade: ")
+endereco = input("Digite seu endereço: ")
+
+print("nome:", nome, sobrenome, "idade:", idade, "endereço:", endereco )
+
+
+
+
+
+
